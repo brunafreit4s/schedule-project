7ae5ec5ee635d888fa90b9f38cbdd3989583f501
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2023-11-27 15:12:00 -0300

    fix aulas django framework

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):    
+    list_display = 'first_name', 'last_name', 'phone', 'email', 'created_date',
+    ordering = 'id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'phone',
--- contact/models.py
+++ contact/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    ...
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=250, blank=True)
+    created_date = models.DateTimeField(auto_created=True, auto_now=True)
+    description = models.TextField(blank=True)
 
+    def __str__ (self) -> str:
+        return f'{self.first_name} {self.last_name}'
