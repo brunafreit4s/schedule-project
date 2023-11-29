5c5ecb0829613103d69f3fa87719bf2a61e1f3fa
Bruna Freitas Almeida <bubufreitas@hotmail.com>
2023-11-29 12:08:02 -0300

    search contact

--- contact/views/contact.py
+++ contact/views/contact.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from contact.models import Contact
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -14,7 +15,6 @@ def index(request):
         context,
     )
 
-# Create your views here.
 def contact(request, contact_id):
     #single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id).first())
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
@@ -25,4 +25,24 @@ def contact(request, contact_id):
         request,
         'contact/contact.html',
         context,
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects.all()\
+        .filter(show=True)\
+        .filter(Q(first_name__icontains=search_value) | 
+                Q(last_name__icontains=search_value)
+            )[0:50]
+    
+    context = {'contacts': contacts, 'site_title': 'Contatos'}
+    
+    return render(
+        request,
+        'contact/index.html',
+        context,        
     )
